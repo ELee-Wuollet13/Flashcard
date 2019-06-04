2aceb663ec66a3ba4cc8951c073dbc47f9712482
Student <example@example.com>
2019-06-04 16:44:48 -0700

    set up files and add content

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#JavaScriptdef").fadeToggle();
+    $("#JavaScript").fadeToggle();
+  });
+
+
+
+  $(".clickable1").click(function() {
+    $("#Operatordef").fadeToggle();
+    $("#Operator").fadeToggle();
+  });
+
+$(".clickable2").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+
+$(".clickable3").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable4").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable5").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable6").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable7").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable8").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable9").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable10").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable11").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+$(".clickable12").click(function() {
+  $("#Variabledef").fadeToggle();
+  $("#Variable").fadeToggle();
+});
+});
